d38409db778db83498e9ace65c3567cb2efa71ac
rathoresachin60 <rathoresachin60@gmail.com>
2025-06-21 19:20:12 +0530

    Add files via upload

---
+++ setRA.sh
@@ -0,0 +1,6 @@
+sudo blockdev --setra 256 /dev/dm-0
+sudo blockdev --setra 256 /dev/dm-1
+sudo blockdev --setra 256 /dev/dm-2
+sudo blockdev --setra 256 /dev/dm-3
+sudo blockdev --setra 256 /dev/dm-4
+sudo blockdev --setra 256 /dev/dm-5
