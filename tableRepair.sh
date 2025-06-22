b0ab5d0cb1bca090bd846cacd9021ad5c211fead
rathoresachin60 <rathoresachin60@gmail.com>
2025-06-22 14:02:42 +0530

    Update tableRepair.sh

--- tableRepair.sh
+++ tableRepair.sh
@@ -2,12 +2,12 @@
 
 if [[ "$3." = "." ]]
 then
-        nodetool -u rsacassandra -pw XCyota01! repair -pr -j 1 $1 $2
+        nodetool -u cassandra -pw ****** repair -pr -j 1 $1 $2
 	if [ "$?" != 0 ]; then
 		echo "ERROR IN REPAIR"
 	fi
 else
-        nodetool -u rsacassandra -pw XCyota01! repair -pr -j $3 $1 $2
+        nodetool -u cassandra -pw ****** repair -pr -j $3 $1 $2
 	if [ "$?" != 0 ]; then
 		echo "ERROR IN REPAIR"
 	fi
