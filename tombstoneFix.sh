a22247bd1f3ddfb05198249a30533c9b09884469
rathoresachin60 <rathoresachin60@gmail.com>
2025-06-22 13:53:58 +0530

    Update tombstoneFix.sh

--- tombstoneFix.sh
+++ tombstoneFix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nodetool -u rsacassandra -pw XCyota01! garbagecollect -- $1 $2
-nodetool -u rsacassandra -pw XCyota01! garbagecollect -- $1 $2
-nodetool -u rsacassandra -pw XCyota01! compact -s -- $1 $2
+nodetool -u rsacassandra -pw ******! garbagecollect -- $1 $2
+nodetool -u rsacassandra -pw ******! garbagecollect -- $1 $2
+nodetool -u rsacassandra -pw ******! compact -s -- $1 $2
 
